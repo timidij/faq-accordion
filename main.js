0a80e24044683e5b11632bfd206630cea62ecb29
Odubo Timidi James <timidiodubo@gmail.com>
2025-04-27 14:14:10 +0100

    revert used istead of block

--- main.js
+++ main.js
@@ -2,62 +2,30 @@ console.log("hello")
 let moreBtn = document.querySelectorAll(".moreBtn")
 let answer = document.querySelectorAll(".answer")
 let minus = document.querySelectorAll(".minus")
-let questions = document.querySelectorAll(".question")
-
-window.onload = function (){
-    console.log("started")
-    if(moreBtn){
-        displayAnswer()
-
-    }
-    
-}
-
-// minus.addEventListener("click", ()=>{
-//    
-//     console.log("answer hidden")
-
-// })
-
+let text = document.querySelectorAll(".question")
 function displayAnswer(){
-    moreBtn.forEach((question,i)=>{
-        // console.log(question)
-       
-        question.addEventListener("click", ()=>{
-            console.log("hellos")
-            hideAnswer()
-            question.style.display = "none"
-            minus[i].style.display = "block"
-            answer[i].style.display = "block"
-
-            minus[i].addEventListener("click", ()=>{
-                console.log('hello check')
-                
-                
-
-                moreBtn[i].style.display = "block"
-                minus[i].style.display= "none"
-            answer[i].style.display = "none"
+    text.forEach((element,i)=>{
+        let isAnswer = false
+        // answer[i].style.display='none'
+        element.addEventListener("click", ()=>{
             
-            
-            })
-            // hideAnswer()
-            if(!answer[i]){
-                minus[i].style.display= "none"
+            if(!isAnswer){
+                answer[i].style.display= 'revert'
+                minus[i].style.display= 'revert'
+                moreBtn[i].style.display= 'none'
+                // answer[i].classList.toggle('mystyle')
+                isAnswer = true
+                
+            }else{
+                answer[i].style.display = 'none'
+                minus[i].style.display= 'none'
+                moreBtn[i].style.display= 'revert'
+
+                isAnswer = false
+
             }
 
         })
-        
     })
 }
-
-function hideAnswer (){
-    minus.forEach((hide, i)=>{
-        moreBtn[i].style.display = "block"
-            minus[i].style.display= "none"
-            answer[i].style.display = "none"
-            
-
-    })
-}
-
+displayAnswer()
